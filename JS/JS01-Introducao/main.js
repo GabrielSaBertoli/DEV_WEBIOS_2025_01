687d9ace13fc48be471a029401e2744c8410400a
GabrielSaBertoli <gabrielbetolisa@gmail.com>
2025-06-03 20:51:34 -0300

    JS

--- JS/JS01-Introducao/main.js
+++ JS/JS01-Introducao/main.js
@@ -2,6 +2,10 @@
 console.log("Hello World") //aoresentar uma mensagem
 console.clear() //limpar
 
-//Variaveis
+//Variaveis - criar um espaço dentro do pc
 var numero1 = 192
 console.log(numero1)
+
+var num1 = 4
+var num2 = 4
+console.log(num1 + num2) //contas de mais sendo executado
