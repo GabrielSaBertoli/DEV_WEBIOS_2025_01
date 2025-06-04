7a609cb5b259e674205e90eb02335135551fa790
GabrielSaBertoli <gabrielbetolisa@gmail.com>
2025-06-04 19:54:16 -0300

    JS

--- JS/JS01-Introducao/main.js
+++ JS/JS01-Introducao/main.js
@@ -17,20 +17,31 @@ console.log(resultado)
 idade = 89 //iniciar a variavel em outra parte do codigo
 console.log(idade)
 
-ano = 1936
+//ano = 1936
 const PI = 3.14
 
 //textos - strings
 //var nome = " " ou '' é lido como texto
-let nome1 = 'Matheus Araujo'
-const nome2  = "Matheus Vinicuius"
-var nome3 = '123456.789' //é um texto, por conta das aspas
+let nome1 = 'Matheus Araujo '
+const nome2  = "Matheus Vinicuius "
+var nome3 = '123456.789 ' //é um texto, por conta das aspas
 console.log(nome1, nome2, nome3) //tudo numa linha ´so, ao inves de copiar
 //console.log(nome2)
 //console.log(nome3)
 
-var //flexivel e com menos regras (posso alterar o valor inicial de uma var e recriar a mesma em outrar partes do código)
+console.log("Olá" + "Papai") //Concatenar 1
+console.log("Aluno " + nome1 + "e aluno" + nome2 + "Tem um total de " + nome3 + "dinheiros")   //concatenar 2
 
-LET//vocÊ não pode recriar a mesma em outras partes do codigo com o mesmo nome, mas pode chamar e alterar o valor inicial
+//Template string
+console.log(`Aluno $(nome) e Aluno $(nome2) Tem um total de ${nome3} dinheiros`) //concatenar 3
 
-CONST//Voce não pode recriar a mesma em outras partes do codigo ee não pode alterar valor inicial dela garantindo a confiança do dado que esta sendo utilizado no decorrer do seu codigo
+
+
+
+
+
+//var //flexivel e com menos regras (posso alterar o valor inicial de uma var e recriar a mesma em outrar partes do código)
+
+//LET//vocÊ não pode recriar a mesma em outras partes do codigo com o mesmo nome, mas pode chamar e alterar o valor inicial
+
+//CONST//Voce não pode recriar a mesma em outras partes do codigo ee não pode alterar valor inicial dela garantindo a confiança do dado que esta sendo utilizado no decorrer do seu codigo
