d1b16bbd32adfb2705a2fbd901eac6d9a8097dfb
GabrielSaBertoli <gabrielbetolisa@gmail.com>
2025-06-03 21:28:09 -0300

    JS

--- JS/JS01-Introducao/main.js
+++ JS/JS01-Introducao/main.js
@@ -1,3 +1,6 @@
+//todo navegador começa a leitura do codigo de cima para baixo
+var idade
+var ano
 //JavaScript
 console.log("Hello World") //aoresentar uma mensagem
 console.clear() //limpar
@@ -8,4 +11,19 @@ console.log(numero1)
 
 var num1 = 4
 var num2 = 4
-console.log(num1 + num2) //contas de mais sendo executado
+var resultado = num1 + num2 //contas de mais sendo executado
+console.log(resultado) 
+
+idade = 89 //iniciar a variavel em outra parte do codigo
+console.log(idade)
+
+ano = 1936
+
+//textos - strings
+//var nome = " " ou '' é lido como texto
+let nome1 = 'Matheus Araujo'
+const nome2  = "Matheus Vinicuius"
+var nome3 = '123456.789' //é um texto, por conta das aspas
+console.log(nome1, nome2, nome3) //tudo numa linha ´so, ao inves de copiar
+//console.log(nome2)
+//console.log(nome3)
