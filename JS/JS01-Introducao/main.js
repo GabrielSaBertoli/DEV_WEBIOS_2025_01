ce431513ce6aba6dfd78963cea6caf04de18a370
GabrielSaBertoli <gabrielbetolisa@gmail.com>
2025-06-03 21:40:19 -0300

    JS

--- JS/JS01-Introducao/main.js
+++ JS/JS01-Introducao/main.js
@@ -1,6 +1,6 @@
 //todo navegador começa a leitura do codigo de cima para baixo
 var idade
-var ano
+const ano = 1936
 //JavaScript
 console.log("Hello World") //aoresentar uma mensagem
 console.clear() //limpar
@@ -18,6 +18,7 @@ idade = 89 //iniciar a variavel em outra parte do codigo
 console.log(idade)
 
 ano = 1936
+const PI = 3.14
 
 //textos - strings
 //var nome = " " ou '' é lido como texto
@@ -27,3 +28,9 @@ var nome3 = '123456.789' //é um texto, por conta das aspas
 console.log(nome1, nome2, nome3) //tudo numa linha ´so, ao inves de copiar
 //console.log(nome2)
 //console.log(nome3)
+
+var //flexivel e com menos regras (posso alterar o valor inicial de uma var e recriar a mesma em outrar partes do código)
+
+LET//vocÊ não pode recriar a mesma em outras partes do codigo com o mesmo nome, mas pode chamar e alterar o valor inicial
+
+CONST//Voce não pode recriar a mesma em outras partes do codigo ee não pode alterar valor inicial dela garantindo a confiança do dado que esta sendo utilizado no decorrer do seu codigo
