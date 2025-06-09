ce0758d1496a7d5331f0e451789d4435eec53082
GabrielSaBertoli <GabrielSaBertoli>
2025-06-09 18:10:23 -0300

    JS 04 finalizado

---
+++ JS/JS02-String/JS02-ExercicioCasa/main.js
@@ -0,0 +1,7 @@
+let frase = "Estamos aprendendo JavaScript e seremos futuros Devs";
+
+console.log(frase[19]); 
+
+console.log(frase.length);
+
+console.log(frase.toUpperCase());
--- JS/JS04-Repeticao/mainExercicio.js
+++ JS/JS04-Repeticao/mainExercicio.js
@@ -1,14 +1,15 @@
-var numeroTabuada = 2
-if(numeroTabuada == 2){
-    for(let i = 0; i <= 10; i++)
-    console.log(` $(mumeroTabuada) x ${i} - ${numeroTabuada * i}`)
-}else if(numeroTabuada == 3){
-    let i = 0
-    while(i <= 10){
-        console.log(` $(mumeroTabuada) x ${i} - ${numeroTabuada * i}`)  
-        i++
+var numeroTabuada = 2;
+
+if (numeroTabuada == 2) {
+    for (let i = 1; i <= 10; i++) {
+        console.log(`${numeroTabuada} x ${i} = ${numeroTabuada * i}`);
     }
-}
-else{
-    console.log("Número incorreto")
+} else if (numeroTabuada == 3) {
+    let i = 1;
+    while (i <= 10) {
+        console.log(`${numeroTabuada} x ${i} = ${numeroTabuada * i}`);
+        i++;
+    }
+} else {
+    console.log("Número incorreto");
 }
