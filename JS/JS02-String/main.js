5de87c0cbc7a3038b68c61750c2ef088785437a2
GabrielSaBertoli <gabrielbetolisa@gmail.com>
2025-06-10 20:46:27 -0300

    JS 02

--- JS/JS02-String/main.js
+++ JS/JS02-String/main.js
@@ -1,2 +1,8 @@
 //"A" //Caracter
 //"ola" // Texto - String
+var texto1 ="Olá IOS"
+//          0 1 2 3 4 5 6 --- Caracteres do "Olá IOS"
+var resultado1 = texto1.charAt(6) //posição dos carácteres na saída do terminal
+console.log(resultado1)
+var resultado2 = texto1[1] //COLCHETES { } SUBSTITUI A charAt
+console.log(resultado2)
