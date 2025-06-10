b86454d98fac882214112d7cc6105892b6404153
GabrielSaBertoli <gabrielbetolisa@gmail.com>
2025-06-10 20:53:09 -0300

    JS 02

--- JS/JS02-String/main.js
+++ JS/JS02-String/main.js
@@ -6,3 +6,8 @@ var resultado1 = texto1.charAt(6) //posição dos carácteres na saída do termi
 console.log(resultado1)
 var resultado2 = texto1[1] //COLCHETES { } SUBSTITUI A charAt
 console.log(resultado2)
+
+
+var lorem = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+
+console.log(lorem.length) //contador de caractéres .lenght
