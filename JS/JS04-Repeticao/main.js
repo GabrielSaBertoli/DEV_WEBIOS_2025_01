3819140f29a32ad59e7990491e6af0f5d46bbe32
GabrielSaBertoli <gabrielbetolisa@gmail.com>
2025-06-12 20:48:27 -0300

    JS 03 terminado

--- JS/JS04-Repeticao/main.js
+++ JS/JS04-Repeticao/main.js
@@ -1,10 +1,18 @@
-let contador = 0
+let contador = 0 //inicializão
 //Enquanto a variavel contador for menor que 10 o while vai apresentar o valor da variavel no console
 while(contador < 10){
     console.log(contador)
     //contador = contador + 1
     //contador +=1
+    contador++
 }
-console.log(1)
-console.log(2)
-console.log(3)
+console.log("Finalizou essa bomba")
+
+// console.log(1)
+// console.log(2)
+// console.log(3)
+
+// variavel inicial; condição; incremento
+for(contador = 0; contador <= 10; contador++){
+console.log(contador)
+}
