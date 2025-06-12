9818b40cef003995cb087bc02d24f6ac6e7acad0
GabrielSaBertoli <gabrielbetolisa@gmail.com>
2025-06-12 20:34:49 -0300

    JS 03 terminado

---
+++ JS/JS04-Repeticao/main.js
@@ -0,0 +1,10 @@
+let contador = 0
+//Enquanto a variavel contador for menor que 10 o while vai apresentar o valor da variavel no console
+while(contador < 10){
+    console.log(contador)
+    //contador = contador + 1
+    //contador +=1
+}
+console.log(1)
+console.log(2)
+console.log(3)
