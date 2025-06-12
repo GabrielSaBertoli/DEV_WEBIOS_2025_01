b93d10158a5ea049c4e2ad1eb7434916b7d0f489
GabrielSaBertoli <gabrielbetolisa@gmail.com>
2025-06-11 21:16:15 -0300

    JS 02 finalizado

---
+++ JS/JS03-Condicional/main.js
@@ -0,0 +1,21 @@
+let atividadeEntregue = false
+if(atividadeEntregue == true){ //true
+console.log('Parabéns, valeu fera')
+}else if(atividadeEntregue == false){ //false
+console.log("Oops, você ainda não entregou sua atividade")
+}else{ //else serve para resposta alternativa,  ou, caso seja um texto que o usuario insira não corresponde as alternativas do código
+    
+    console.log("Você inseriu uma informação inválida") 
+}
+
+
+
+
+
+
+
+
+
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+//xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
