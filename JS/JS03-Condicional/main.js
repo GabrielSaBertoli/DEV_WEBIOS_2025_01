fdcd7108afa812410118e0f70c611b283974b00f
GabrielSaBertoli <gabrielbetolisa@gmail.com>
2025-06-11 21:34:02 -0300

    JS 02 finalizado

--- JS/JS03-Condicional/main.js
+++ JS/JS03-Condicional/main.js
@@ -1,11 +1,24 @@
 let atividadeEntregue = false
+let diaFinalEntrega = 19
+let mensagemParabens = 'Parabéns, valeu fera'
+
 if(atividadeEntregue == true){ //true
-console.log('Parabéns, valeu fera')
-}else if(atividadeEntregue == false){ //false
+console.log("mensagemPabéns")
+}
+
+if(diaFinalEntrega <= 13){
+console.log("Entregou a atividade na data e sua nota é 10!")
+}
+
+else{console.log("Entregou fora da data e sera retirado 2 pontos")
+}
+
+else if(atividadeEntregue == false){ //false
 console.log("Oops, você ainda não entregou sua atividade")
-}else{ //else serve para resposta alternativa,  ou, caso seja um texto que o usuario insira não corresponde as alternativas do código
-    
-    console.log("Você inseriu uma informação inválida") 
+}
+
+else{ //else serve para resposta alternativa,  ou, caso seja um texto que o usuario insira não corresponde as alternativas do código
+    console.log("Você inseriu uma informação inválida")
 }
 
 
