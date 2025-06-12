babf3a5dabafff3f2ea14cfa89f4fb55eccde912
GabrielSaBertoli <gabrielbetolisa@gmail.com>
2025-06-12 20:09:49 -0300

    JS 03 terminado

---
+++ JS/JS03-Condicional/JS03-ExercicioCasa/main.js
@@ -0,0 +1,8 @@
+var numero1 = 4
+var numero2 = 2
+
+    if (numero1 > numero2){
+        console.log("numero1 é maior que numero2")
+    }else if ( numero1 < numero2) {
+        console.log("numero 1 é menor que numero 2")
+    }
