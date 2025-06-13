3ea9af610ce1cfc16c2cec5f14e22a567c6250fe
GabrielSaBertoli <gabrielbetolisa@gmail.com>
2025-06-12 21:11:18 -0300

    JS 03 terminado

---
+++ JS/JS04-Repeticao/mainExercicio.js
@@ -0,0 +1,14 @@
+var numeroTabuada = 2
+if(numeroTabuada == 2){
+    for(let i = 0; i <= 10; i++)
+    console.log(` $(mumeroTabuada) x ${i} - ${numeroTabuada * i}`)
+}else if(numeroTabuada == 3){
+    let i = 0
+    while(i <= 10){
+        console.log(` $(mumeroTabuada) x ${i} - ${numeroTabuada * i}`)  
+        i++
+    }
+}
+else{
+    console.log("Número incorreto")
+}
