cbbf48434b9978dd8c8b9d92810d9f507585e018
GabrielSaBertoli <GabrielSaBertoli>
2025-06-15 20:26:22 -0300

    JS 01 finalizado

---
+++ JS/JS01-Introducao/JS01-ExercicioCasa/main.js
@@ -0,0 +1,8 @@
+let nota1 = 7.5;
+let nota2 = 8.0;
+let nota3 = 9.0;
+let nota4 = 6.5;
+
+let media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+console.log(`A média do aluno com as 4 notas é: ${media}`);
